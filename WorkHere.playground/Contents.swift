4e8a022408e45e2f85c19e9ebe31ece5d40d30c5
cramsayiv <37635288+cramsayiv@users.noreply.github.com>
2019-10-03 18:46:43 -0500

    Complete Homework 3

--- WorkHere.playground/Contents.swift
+++ WorkHere.playground/Contents.swift
@@ -10,3 +10,38 @@ import Foundation
 // decode those 50 words into an array of strings
 // print the array of words, but with each word reversed
 // print the average number of characters per word
+
+func reversedList (myList: [String]) -> [String] {
+    var reversedList = [String] ()
+    for words in myList {
+      reversedList.append(String(words.reversed()))
+    }
+  return reversedList
+}
+
+func avgChars (myList: [String]) -> Int {
+    var totalChars = 0
+    for words in myList {
+        totalChars += words.count
+    }
+    return (totalChars/myList.count)
+}
+
+func printList(myList: [String]) {
+  for words in myList {
+    print(words)
+  }
+}
+
+let urlString = "https://api.noopschallenge.com/wordbot?count=50"
+
+if let url = URL (string: urlString) {
+    let request = URLRequest(url: url)
+    let session = URLSession(configuration: .ephemeral)
+    let task = session.dataTask(with: request) { (data, response, error) in
+        let myMainList = try! JSONDecoder().decode (Words.self, from: data!)
+        printList(myList: reversedList(myList: myMainList.names))
+        print(avgChars(myList: myMainList.names))
+    }
+    task.resume()
+}
